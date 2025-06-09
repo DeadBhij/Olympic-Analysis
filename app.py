7d18d0b2531d4e298af5a6d14f25421fdd9b15bb
Sanidhya Shukla <sndyshukla022@gmail.com>
2025-06-10 05:05:59 +0530

    update

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import seaborn as sns
 import plotly.figure_factory as ff
 
 
-df = pd.read_csv(r'C:\Users\WiZ\Documents\olympic data analysis\athlete_events.csv')
-region_df = pd.read_csv(r'C:\Users\WiZ\Documents\olympic data analysis\noc_regions.csv')
+df = pd.read_csv(r'C:\Users\WiZ\Documents\Olympic Analysis\athlete_events.csv')
+region_df = pd.read_csv(r'C:\Users\WiZ\Documents\Olympic Analysis\noc_regions.csv')
 
 df = preprocessor.preprocess(df,region_df)
 
